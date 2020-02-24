52098fa767c18a673a72c1d1124da56dd97a3ff3
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-02-24 11:17:55 +0800

    basic functionality built

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  // const [character, setCharacter] = useState();
+  // const paramItemId = props.match.params.id;
+
+  // useEffect(() => {
+  //   const id = paramItemId;
+  //   axios
+  //     .get(`https://rickandmortyapi.com/api/character/${id}`)
+  //     .then(response => {
+  //       setCharacter(response.data.results);
+  //     })
+  //     .catch(error => error);
+
+  // }, [paramItemId]);
+
+  return (
+    <div>
+      {/* <span>todo: character</span> */}
+      <h2>{props.character.name}</h2>
+      <p>Status: {props.character.status}</p>
+      <p>Species: {props.character.species}</p>
+      <img src={props.character.image} alt="character img" />
+    </div>
+  );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,23 @@
 import React from "react";
+import {Route} from "react-router-dom";
+import CharacterList from "./CharacterList";
+import CharacterCard from "./CharacterCard";
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
+        { /* <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
-        />
+        /> */}
       </header>
+      <Route exact path="/" component={CharacterList} />
+      <Route path="/characters/:id" render={(renderProps) => {
+        return <CharacterCard {...renderProps} />
+      }} />
     </section>
   );
 }
